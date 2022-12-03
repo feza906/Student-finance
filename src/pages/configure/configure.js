261a6e3bbbd7633dde2bd96c50156dfa4ad00c65
Fezacodes <82339173+feza906@users.noreply.github.com>
2022-12-04 01:08:01 +0530

    Update configure.js

--- src/pages/configure/configure.js
+++ src/pages/configure/configure.js
@@ -1,5 +1,5 @@
 var myHeaders = new Headers();
-myHeaders.append("apikey", "EOpMX4gxrcBxWspxVUe3E5stAUh5x5Be");
+myHeaders.append("apikey", APIKEY);
 
 export var requestOptions = {
   method: 'GET',
