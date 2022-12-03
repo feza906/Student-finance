5fd2c5d32645babfa3699fa2df2c44e778f27ad4
feza906 <fatimafeza04@gmail.com>
2022-12-03 19:52:38 +0530

    message

--- src/pages/Calculate.js
+++ src/pages/Calculate.js
@@ -1,9 +1,46 @@
 import React from 'react'
+import { useState } from 'react'
 
 export const Calculate = () => {
+  const [total,setTotal] = useState(0);
+  
   return (
-    <div className='mt-16'>
-        <h1 className='ml-6 text-start text-xl'>Worried about so many expenses and confused at the same time,Don't worry we will help you by converting the currencies ,and you could compare the prices and decide what's best for you.</h1>
+    <div className=''>
+        <h1 className='mt-16 ml-6 text-start text-xl text-blue-800'>Worried about so many expenses and confused at the same time,Don't worry we will help you by converting the currencies ,and you could compare the prices and decide what's best for you.</h1>
+        <section style = {{backgroundImage: `url(https://images.unsplash.com/photo-1625225233840-695456021cde?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8Y2FsY3VsYXRvcnxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=1000&q=60)`}} className='bg-cover bg-center h-screen p-8 px-16 xl:px-96 py-16 mt-8'>
+        <form className= 'bg-white text-center py-16 rounded-lg uppercase rounded-xl'>
+        <h1 className='text-2xl text-blue-600 mb-4'>Start Converting</h1>
+        
+          <label className='text-xl'>Select the Currency</label>
+          <br/>
+          <select className='ml-8 mt-2 px-2 mb-2'>
+            <option value="volvo">INR</option>
+            <option value="saab">AUD</option>
+            <option value="opel">CAD</option>
+            <option value="audi">EUR</option>
+            <option value="audi">USD</option>
+
+          </select>
+          <br/>
+          <label >Tution Fees</label><br/>
+          <input type={'number'}  className="border-2 border-black rounded-xl px-4" onChange={(e) => setTotal(total + e.target.value)} required/>
+          <br/>
+          <label >Study Materials</label><br/>
+          <input type={'number'} className="border-2 border-black rounded-xl px-4"/>
+          <br/>
+          <label >Accommodtion</label><br/>
+          <input type={'number'} className="border-2 border-black rounded-xl px-4"/>
+          <br/>
+          <label >Transportation</label><br/>
+          <input type={'number'} className="border-2 border-black rounded-xl px-4"/>
+          <br/>
+          <label >Grocery</label><br/>
+          <input type={'number'} className="border-2 border-black rounded-xl px-4"/>
+          <br/>
+          <label >Total</label><br/>
+          <input type={'number'} className="border-2 border-black rounded-xl px-4" value={total}/>
+        </form>
+        </section>
     </div>
   )
 }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 export const Home = () => {
   return (
-    <div className='flex flex-col justify-center'>
+    <div className='flex flex-col justify-center items-center'>
       <h1 className='border-2 mt-20 text-center text-blue-900 text-3xl uppercase font-bold p-4'>Here is the service you deserve</h1>
       <img src='images/img1.png' height='300px' width = '450px' alt="image" className="my-20 mx-20"/>
     </div>
