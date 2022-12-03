0926b8e566c7944b4a5001a29998592df6a5bcb4
Aqib Javid Bhat <aqib.m31.dev@gmail.com>
2022-12-03 17:38:30 +0530

    Modify Homepage

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,6 +2,9 @@ import React from 'react'
 
 export const Home = () => {
   return (
-    <div>Home</div>
+    <div className='flex flex-col justify-center'>
+      <h1 className='border-2 mt-20 text-center text-blue-900 text-3xl uppercase font-bold p-4'>Here is the service you deserve</h1>
+      <img src='images/img1.png' height='300px' width = '450px' alt="image" className="my-20 mx-20"/>
+    </div>
   )
 }
